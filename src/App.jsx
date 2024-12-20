8dbe177f2090cd8327d56ad78829d68ac279d004
tanmay2301 <tanmayshukla.work@gmail.com>
2024-12-20 23:22:12 +0530

    Fixed blank page no rendering issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css'
 import Header from './components/Header'
 import SideBar from './components/SideBar'
-import Cards from "./Cards"
+
 
 function App() {
   return(
     <div className="main">
       <SideBar />
-      <Header />
-      
+      <Header />  
     </div>
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
