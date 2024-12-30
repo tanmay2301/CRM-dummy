1130b4766b81ecb217b7698ad90dfa771b011e5e
tanmay2301 <tanmayshukla.work@gmail.com>
2024-12-30 20:24:17 +0530

    Add charts to analytics

--- src/custom-components/Header.jsx
+++ src/custom-components/Header.jsx
@@ -1,10 +1,14 @@
+import {Link} from 'react-router-dom'
+
 function Header(){
     return(
         <>
             <nav className="navbar">
                 <div className="navbar-content">
                     <i className="fa-solid fa-chart-line"></i>
-                    <h1>LaunchSync</h1>
+                    <Link to="/Dashboard"  style={{ textDecoration: 'none' }}> 
+                        <h1>LaunchSync</h1>
+                    </Link>
                 </div>
                 <hr /> 
             </nav>  
