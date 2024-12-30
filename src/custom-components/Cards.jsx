69b347539afba17bb4432b417984f4220f9fdbf5
tanmay2301 <tanmayshukla.work@gmail.com>
2024-12-31 01:24:29 +0530

    Modify Dashboard Cards using ShadCN

--- src/custom-components/Cards.jsx
+++ src/custom-components/Cards.jsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react"
 import database from "../api"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  
 
 export default function Cards() {
     const [cards, setCards] = useState(database.data)
@@ -7,12 +16,22 @@ export default function Cards() {
     return (
         <section>
             {cards.map(card => (
-                <div className="card" key={card.id}>
-                    <i className={card.iconClass}></i>
-                    <h3>{card.title}</h3>
-                    <h1>{card.count}</h1>
-                    <h5>{card.description}</h5>
-                </div>
+                <Card className = "hover:-translate-y-1 hover:shadow-lg transition-transform">
+                    <CardHeader className = "h-[140 px] w-[278px]">
+                        <div className="flex items-center">
+                            <CardTitle className="w-[200px] text-2xl"> {card.title}</CardTitle>
+                            <div className="flex items-center pl-[20px] text-zinc-700">
+                                <i className={card.iconClass}></i>
+                            </div>
+                        </div>
+                    <CardDescription className="pt-[5px]"><p className="text-xs font-normal">{card.description}</p></CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <p className="text-3xl font-bold">{card.count}</p>
+                    </CardContent>
+                </Card>
+              
+                
             ))}
         </section>
     )
